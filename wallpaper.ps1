94fe17d7ad751731b0905197d03af1f69102d53a
uefibootkit <156601330+uefibootkit@users.noreply.github.com>
2026-01-11 14:05:54 +0300

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,10 +1,7 @@
-# Duvar kagidi dosya yolu
-$path = "cd C:\Users\L\AppData\Roaming\.minecraft\Mia_Khalifa_pornstar-1402223.jpg"
+$path = "C:\Users\L\Pictures\wallpaper.jpg"
 
-# Registry ayari
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallPaper -Value $path
 
-# Duvar kagidini zorla yenile
 Add-Type @"
 using System.Runtime.InteropServices;
 public class Wallpaper {
